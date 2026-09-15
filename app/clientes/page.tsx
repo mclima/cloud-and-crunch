b60fbd3ca2db41091a1af6dddaf8bb66075efc28
Maria Lima <mariaclimany@gmail.com>
2026-09-15 18:52:58 +0100

    revise pricing

--- app/clientes/page.tsx
+++ app/clientes/page.tsx
@@ -50,7 +50,7 @@ const products = [
     format: 'Tacinha',
     capacity: '230ml',
     minQuantity: '9',
-    price: '2.25',
+    price: '2.50',
     image: '/images/natas-do-ceu.webp',
   },
   {
